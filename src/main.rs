4ce2ef818e39a2baa06a435d54d580bfba0c8c52
Tim Freund <tim@freunds.net>
2019-12-15 12:45:14 -0500

    That actually works.  Not great yet:  there's some clicking when transitioning between notes, but that's not really the point of this exercise.

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,7 @@ extern crate jack;
 use std::{thread, time};
 
 
-fn midi_to_frequency(midi_note: i8) -> f64 {
+fn midi_to_frequency(midi_note: u8) -> f64 {
     let midi_f = midi_note as f64;
     return 2_f64.powf((midi_f - 69.0) / 12.0) * 440.0;
 }
@@ -17,7 +17,7 @@ fn main() {
     let midi_in = jack_client.register_port("midi_in", jack::MidiIn::default()).unwrap();
     println!("{}", midi_to_frequency(67));
 
-    let frequency = midi_to_frequency(67);
+    let mut frequency = midi_to_frequency(67);
     let sample_rate = jack_client.sample_rate();
     let frame_t = 1.0 / sample_rate as f64;
     let mut time = 0.0;
@@ -35,7 +35,10 @@ fn main() {
 
             let midi_data = midi_in.iter(ps);
             for raw_midi in midi_data {
-                println!("{}", hex::encode(raw_midi.bytes))
+                println!("{}", hex::encode(raw_midi.bytes));
+                let midi_note = raw_midi.bytes[1];
+                frequency = midi_to_frequency(midi_note);
+                println!("{}", frequency);
             }
             jack::Control::Continue
         },
