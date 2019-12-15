baa9317c4c25064e19d1060159da928c870bef81
Tim Freund <tim@freunds.net>
2019-12-15 01:32:17 -0500

    Slight variation on the sine.rs jack example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,43 @@
+// extern crate crossbeam_channel;
+extern crate jack;
+
+// use crossbeam_channel::bounded;
+use std::{thread, time};
+
+
+fn midi_to_frequency(midi_note: i8) -> f64 {
+    let midi_f = midi_note as f64;
+    return 2_f64.powf((midi_f - 69.0) / 12.0) * 440.0;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let(jack_client, _status) = jack::Client::new("tone_generator", jack::ClientOptions::NO_START_SERVER).unwrap();
+    let mut audio_out = jack_client.register_port("audio_out", jack::AudioOut::default()).unwrap();
+    let midi_int = jack_client.register_port("midi_in", jack::MidiIn::default()).unwrap();
+    println!("{}", midi_to_frequency(67));
+
+    let frequency = midi_to_frequency(67);
+    let sample_rate = jack_client.sample_rate();
+    let frame_t = 1.0 / sample_rate as f64;
+    let mut time = 0.0;
+
+    let generate_tone = jack::ClosureProcessHandler::new(
+        move |_: &jack::Client, ps: &jack::ProcessScope| -> jack::Control {
+            let out = audio_out.as_mut_slice(ps);
+
+            for v in out.iter_mut() {
+                let x = frequency * time * 2.0 * std::f64::consts::PI;
+                let y = x.sin();
+                *v = y as f32;
+                time += frame_t;
+            }
+            jack::Control::Continue
+        },
+    );
+
+    let aclient = jack_client.activate_async((), generate_tone).unwrap();
+    let sleep_period = time::Duration::from_millis(500);
+    loop {
+        thread::sleep(sleep_period);
+    }
 }
